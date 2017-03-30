e20081f28cdbfcc4f445f1197e2cf0bcdd661a16
AristidesMini <AristidesMini@172.30.28.78>
2017-03-30 17:36:19 +0100

    primeiro comit

---
+++ Trabalho1/src/Trabalho1.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Trabalho1.cpp
+// Author      : Nuno Silva
+// Version     :
+// Copyright   : no copyrino
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctype.h>
+using namespace std;
+
+
+int main() {
+	cout << "faking rash b" << endl; // prints faking rash b
+	return 0;
+}
