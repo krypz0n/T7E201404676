d51fa177e4659ecd87b4a84fa7e88fbf5bd6e626
AristidesMini <AristidesMini@172.30.28.78>
2017-03-30 17:44:57 +0100

    Struct's criados, fazer funçoes de management das structs e file I/O

--- Trabalho1/src/Trabalho1.cpp
+++ Trabalho1/src/Trabalho1.cpp
@@ -1,9 +1,9 @@
 //============================================================================
 // Name        : Trabalho1.cpp
-// Author      : Nuno Silva
+// Author      : Nuno Silva Turma 7 up201404676
 // Version     :
 // Copyright   : no copyrino
-// Description : Hello World in C++, Ansi-style
+// Description : Trabalho 1
 //============================================================================
 
 #include <iostream>
@@ -12,8 +12,26 @@
 #include <ctype.h>
 using namespace std;
 
+struct linha{
+	int ID;
+	int frequencia;
+	string paragens[];
+	int temposParagens[];
+
+};
+
+struct condutor{
+	string nome;
+	int ID;
+	int horasMaxTurno;
+	int horasMaxSemana;
+	int horasDescansoTurno;
+};
 
 int main() {
-	cout << "faking rash b" << endl; // prints faking rash b
+	condutor Manuel;
+	Manuel.ID=500;
+	Manuel.nome="Manuel Silva";
+	cout << Manuel.nome << " " << Manuel.ID << endl;
 	return 0;
 }
